011db49f436b0727aeb6674d242c036fccc50a27
pallavi <pallavi.poddar@neosoftmail.com>
2021-09-22 17:04:54 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./components/styles.css"
+import { BrowserRouter, Route } from "react-router-dom";
+import Cart from "./components/Cart";
+import Header from "./components/Header";
+import Home from "./components/Home";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <div>
+        <Route path="/" exact><Home /></Route>
+        <Route path="/cart" exact><Cart /></Route>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
